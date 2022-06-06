3c4390692e2ab00580d6912db6f23f87ea3b962a
laalex <laalex512@gmail.com>
2022-06-06 19:14:03 +0300

    Add Ex005 ConditionIfElse

---
+++ Example005ConditionIfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What's your name?");
+string userName=Console.ReadLine();
+if (userName.ToLower()=="masha"){
+    Console.WriteLine("WOW! This is Masha!");
+}
+else{
+    Console.Write("Hello, ");
+    Console.WriteLine(userName);
+}
