668f0473fd6187a000d5ac09bbc917cf2e20395b
laalex <laalex512@gmail.com>
2022-06-07 20:20:45 +0300

    Add Ex008 IntroMethod

--- Example003Sum/Program.cs
+++ Example003Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿int numA=new Random().Next(1,10);
+﻿int numA = new Random().Next(1, 10);
 Console.WriteLine(numA);
-int numB=new Random().Next(1,10);
+int numB = new Random().Next(1, 10);
 Console.WriteLine(numB);
-Console.WriteLine(numA+numB);
+Console.WriteLine(numA + numB);
---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿int a1=23;
+int b1=26;
+int c1=48;
+int a2=15;
+int b2=7;
+int c2=84;
+int a3=97;
+int b3=12;
+int c3=15;
+
+int Max(int a,int b,int c) {
+    int result=a;
+    if (b>result) result=b;
+    if (c>result) result=c;
+    return result;
+}
+
+int max=Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3)
+);
+Console.WriteLine(max);
